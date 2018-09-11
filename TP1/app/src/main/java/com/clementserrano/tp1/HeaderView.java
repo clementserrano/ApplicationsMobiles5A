da127afb7fd9c424f63e36e79551a09e98c18c13
Clément Serrano <clementserrano3@gmail.com>
2018-09-11 15:44:59 +0200

    TP1 11/09 Custom Controler

---
+++ TP1/app/src/main/java/com/clementserrano/tp1/HeaderView.java
@@ -0,0 +1,49 @@
+package com.clementserrano.tp1;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class HeaderView extends LinearLayout {
+
+    //private TextView movieTitle;
+
+    public HeaderView(Context context) {
+        super(context);
+    }
+
+    public HeaderView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HeaderView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public HeaderView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        this.initialize(context, attrs);
+    }
+
+    private void initialize(Context context, AttributeSet attrs) {
+        inflate(context, R.layout.header, this);
+        /*this.movieTitle = findViewById(R.id.movieTitle);
+
+        if (attrs != null) {
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.HeaderView, 0, 0);
+
+            String movieTitle = array.getString(R.styleable.HeaderView_movieTitle);
+            if (this.movieTitle == null) {
+                this.movieTitle.setText(movieTitle);
+            }
+
+            array.recycle();
+        }*/
+    }
+}
--- TP1/app/src/main/java/com/clementserrano/tp1/MainActivity.java
+++ TP1/app/src/main/java/com/clementserrano/tp1/MainActivity.java
@@ -1,7 +1,13 @@
 package com.clementserrano.tp1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
